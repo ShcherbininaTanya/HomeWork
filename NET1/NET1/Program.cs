c5c386fd375358bd103960f7d86ebac3d029b86a
ShcherbininaTanya <44109215+ShcherbininaTanya@users.noreply.github.com>
2018-10-13 11:45:59 +0400

    ДЗ1

---
+++ NET1/NET1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Работа с консолью:");
+            Console.WriteLine("Введите координату X");
+            decimal xx = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату Y");
+            decimal yy = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("{0,-15} {1,-5}", "X: " + xx, "Y: " + yy);
+
+            Console.WriteLine("Работа с файлом:");
+            using (StreamReader file = new StreamReader(@"C:\Tanya\Универ\Net\Проекты\text.txt"))
+            {
+                while (!file.EndOfStream)
+                {
+                    string[] mass = file.ReadLine().Split(' ');
+                    decimal x = decimal.Parse(mass[0]);
+                    decimal y = decimal.Parse(mass[1]);
+                    Console.WriteLine("{0,-15} {1,-5}", "X: "+x, "Y: "+y);
+                }
+                Console.Write("Для продолжения нажмите на любую клавишу");
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
