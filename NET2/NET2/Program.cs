56c900fd6cd2c8416ed0c3b2a75009c99a0a2648
ShcherbininaTanya <44109215+ShcherbininaTanya@users.noreply.github.com>
2018-10-20 13:36:21 +0400

    ДЗ2

---
+++ NET2/NET2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            Double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите корень какой степени вы хотите вычислить");
+            Double n = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите точность");
+            int t = int.Parse(Console.ReadLine());
+            Console.WriteLine("Результат:");
+            double rez = SqrtN(n, a);
+            Console.WriteLine(Math.Round(rez, t));
+            Console.WriteLine("Стандартная функция: " + Math.Round(Math.Pow(a, 1/n), t));
+            Console.Write("Для продолжения нажмите на любую клавишу...");
+            Console.ReadKey(true);
+        }
+
+        static double Stepen(double a, int st)
+        {
+            double result = 1;
+            for (int i = 0; i < st; i++) result *= a;
+            return result;
+        }
+
+        static double SqrtN(double n, double A, double eps = 0.0001)
+        {
+            var x0 = A/n;
+            var x1 = (1/n)*((n-1)*x0+A/Stepen(x0,(int)n-1));
+
+            while (Math.Abs(x1-x0)>eps)
+            {
+                x0 = x1;
+                x1 = (1/n)*((n-1)*x0+A/Stepen(x0,(int)n-1));
+            }
+
+            return x1;
+        }
+    }
+}
---
+++ NET2/zadanie2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie2
+{
+    class Program
+    {
+        static string DECtoBIN(int n)
+        {
+            string s = "";
+            int del;
+            while (n > 0)
+            {
+               del = n % 2;
+                n = n / 2;
+                s=del+s;
+            }
+            return s;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число в десятичной системе:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Число " + n + " в двоичной системе счисления:");
+            Console.WriteLine(DECtoBIN(n));
+            string BinaryCode = Convert.ToString(n, 2);
+            Console.WriteLine("Стандартная функция: " + BinaryCode);
+            Console.Write("Для продолжения нажмите на любую клавишу...");
+            Console.ReadKey(true);
+        }
+        
+    }
+}
